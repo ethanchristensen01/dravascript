38b1edee31d80c931a36b3ba94d47d45c430dd30
EMC <ethanchristensen01@gmail.com>
2014-05-23 13:23:40 -0600

    Improved storage
    Yup, now we have improved storage, that can hold *almost* any type of
    data. Also tested out audio, and added a basic method to the Audio
    class called stop.

--- EMCscript.js
+++ EMCscript.js
@@ -1,6 +1,8 @@
 //Coding library
 var m_canvas = document.getElementById("canvas");
 var ct = m_canvas.getContext("2d");
+var b1 = document.getElementById("b1");
+var b2 = document.getElementById("b2");
 var mouseIsPressed = false;
 var mouseMoved = false;
 var mouseX = 0;
@@ -71,7 +73,10 @@ var stroke = function(r,g,b,a){
     }
 };
 var strokeWeight = function(n){
-    ct.lineWidth = n;
+    ct.lineWidth = ""+n+"";
+}
+var strokeCap = function(tp){
+    ct.lineCap = tp;
 }
 var noFill = function(){
     ct.fillStyle = "rgba(0,0,0,0)";
@@ -129,9 +134,9 @@ var textFont = function(f,n){
     ct.font = TEXTSIZE+FONT;
 }
 //Text breaks. http://stackoverflow.com/a/21574562
-var text = function(text, x, y) {
+var text = function(t, x, y) {
   var lineHeight = ct.measureText("M").width * 1.2;
-  var lines = text.split("\n");
+  var lines = t.split("\n");
   for (var i = 0; i < lines.length; ++i) {
     ct.fillText(lines[i], x, y);
     ct.strokeText(lines[i], x, y);
@@ -174,15 +179,42 @@ var asin = function(n){
     }
     return Math.asin(n);
 }
+var sqrt = function(n){
+    return(Math.sqrt(n));
+}
+var pow = function(n,xp){
+    return(Math.pow(n,xp))
+}
+var abs = function(n){
+    return(Math.abs(n));
+}
+var dist = function(x1,y1,x2,y2){
+    var w = abs(x1-x2);
+    var h = abs(y1-y2);
+    return(sqrt(pow(w,2)+pow(h,2)));
+}
 var getItem = function(key){
-    return(localStorage.getItem(key));
+    return(JSON.parse(localStorage.getItem(key)));
 }
 var setItem = function(key,val){
-    localStorage.setItem(key,val);
+    localStorage.setItem(key,JSON.stringify(val));
 }
 var deleteItem = function(key){
-    localStorage.deleteItem(key);
+    localStorage.removeItem(key);
 }
+var canvasSize = function(w,h){
+    m_canvas.width = w;
+    m_canvas.height = h;
+    canvasWidth = w;
+    canvasHeight = h;
+}
+Audio.prototype.stop = function(){
+    this.pause();
+    this.currentTime = 0;
+}
+strokeCap("round");
+stroke(0,0,0);
+fill(255,255,255);
 window.onload=function(){
     if(draw!==undefined){
         setInterval(draw,1000/30);
